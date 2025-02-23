4f9e8ee4c62014696e834af2f680c0bacb9140f9
JUAN DAVID SAAVEDRA REYES <90642323+Juancho2406@users.noreply.github.com>
2025-02-23 10:01:14 -0500

    deploy app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import { TamaguiProvider, View } from '@tamagui/core'
+import { config} from './tamagui.config' // your configuration
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-    </div>
-  );
+  <TamaguiProvider config={config}>
+    <View width={200} height={200} background={"rgba(0,0,0,0.2)"}/>
+  </TamaguiProvider>
+  )
 }
-
-export default App;
